637609cf157e401125c5230e032841f962c3f89a
Lafaietepedro <lafaietepedro3@gmail.com>
2024-09-16 17:45:13 -0300

    16_09 class update

--- inicio.py
+++ inicio.py
@@ -4,8 +4,25 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/login',methods=['POST'])
+def logar():
+    return render_template('login.html')
+
+@app.route('/autenticar',methods=['GET','POST'])
+def autenticar():
+    if request.method == 'POST':
+        if request.form['senha'] == '123' and request.form['usuario'] == 'Joao':
+            return render_template('senia.html')
+        else:
+            msg = 'Erro na autenticação'
+            return render_template('login.html',msg=msg)
+
+@app.route('/senia',methods=['GET','POST'])
+def abrir_assistente():
     audio_path = None
     if request.method == 'POST':
         texto = request.form['texto']
@@ -16,7 +33,7 @@ def index():
         audio_path = "static/audio_exemplo.mp3"
 
         tts.save(audio_path)
-    return render_template('index.html', audio_path=audio_path)
+    return render_template('senia.html', audio_path=audio_path)
     
 if __name__ == '__main__':
     app.run(debug=True)
